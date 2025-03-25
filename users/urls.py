b83af75f4f990670fbf43faba03a483e1cb3a724
ku93 <tkachev1993adg@yandex.ru>
2025-03-25 08:24:37 +0300

    task5

--- users/urls.py
+++ users/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path("email-confirm/<str:token>/", email_verification, name="email-confirm"),
     path('password_reset/', PasswordResetView.as_view(template_name='password_reset.html', success_url=reverse_lazy("urls:password_reset_done"),), name='password_reset'),
     path('password_reset/done/', PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/<str:token>/', PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
